9f2d49789b08aa838427febd85fbec16f2f82e04
GoldenCodex <evan.norman@me.com>
2021-02-09 22:31:26 -0700

    bruh

---
+++ brainKiller/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace brainKiller
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+            => await Startup.RunAsync(args);
+    }
+}
---
+++ brainKiller/Services/CommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace brainKiller.Services
+{
+    public class CommandHandler
+    {
+        public static IServiceProvider _provider;
+        public static DiscordSocketClient _discord;
+        public static CommandService _commands;
+        public static IConfigurationRoot _config;
+        public CommandHandler(DiscordSocketClient discord, CommandService commands, IConfigurationRoot config, IServiceProvider provider)
+        {
+            _provider = provider;
+            _config = config;
+            _discord = discord;
+            _commands = commands;
+
+
+            _discord.Ready += OnReady;
+        }
+
+        private Task OnReady()
+        {
+            Console.WriteLine($"Connected as {_discord.CurrentUser.Username}#{_discord.CurrentUser.Discriminator}");
+            return Task.CompletedTask;
+        }
+    }
+}
