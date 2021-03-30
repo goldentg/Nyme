154d3f89e7be574f797266e1d8f5ea109861f226
GoldenCodex <evan.norman@me.com>
2021-03-29 21:24:07 -0600

    smol fix

--- brainKiller/Services/CommandHandler.cs
+++ brainKiller/Services/CommandHandler.cs
@@ -201,7 +201,7 @@ namespace brainKiller.Services
         {
             if (command.IsSpecified && !result.IsSuccess)
                 await context.Channel.SendMessageAsync(
-                    $"Error: {result}.\n*If you think this error is a bug or issue with the bot please let me know on the [support server](https://discord.gg/a5SmPbSGEJ)*");
+                    $"Error: {result}.\n*If you think this error is a bug or issue with the bot please let me know on the support server https://discord.gg/a5SmPbSGEJ*");
         }
     }
 }
